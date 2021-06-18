83633e9a874b3b5a6c46830aea31ac8a681757ee
Alvaro Canepa <bfpdevel@gmail.com>
2021-06-18 15:30:39 -0300

    - Add missing properties on UserAddressData

--- src/types/UserAddress.d.ts
+++ src/types/UserAddress.d.ts
@@ -1,20 +1,23 @@
 export interface UserAddressData {
-  id?: number;
-  user_id?: number;
-  type?: string;
-  country?: string | number;
-  state?: string | number;
-  city?: string | number;
-  street?: string;
-  house?: string | number;
-  building?: string;
-  flat?: string | number;
-  floor?: string | number;
-  address1?: string;
-  address2?: string;
-  postcode?: string;
-  created_at?: string;
-  updated_at?: string;
+  id: number;
+  user_id: number;
+  type: string;
+  country: string | number;
+  state: string | number;
+  city: string | number;
+  country_text: string;
+  state_text: string;
+  city_text: string;
+  street: string;
+  house: string | number;
+  building: string;
+  flat: string | number;
+  floor: string | number;
+  address1: string;
+  address2: string;
+  postcode: string;
+  created_at: string;
+  updated_at: string;
 }
 
 export interface UserAddressUpdateResponse {
