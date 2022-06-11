9e196455c2fdb07012ece8b6301ebb83cee9b57f
Alvaro Canepa <bfpdevel@gmail.com>
2022-06-11 15:05:21 -0300

    v2.0.1 - Add missing attributes to UserData

--- src/types/User.d.ts
+++ src/types/User.d.ts
@@ -8,12 +8,22 @@ export interface UserData {
   last_name: string;
   middle_name: string;
   phone: string;
+  phone_short: string;
   phone_list: string[];
-  socialite_token: string[];
   avatar: string;
   property: Record<string, any>;
   address: UserAddressData[];
   role: string;
   created_at?: string;
   updated_at?: string;
+  deleted_at?: string;
+  activation_code: string;
+  persist_code: string;
+  reset_password_code: string;
+  permissions: string;
+  is_activated: boolean;
+  activated_at: string;
+  last_login: string;
+  is_superuser: boolean;
+  active_currency_code: string;
 }
