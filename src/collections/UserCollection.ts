7366c206b43f5cfd2f5e8d3d125ba9ca42230753
Alvaro Canepa <bfpdevel@gmail.com>
2022-12-18 10:02:54 -0300

    - Add prettier config - Add list method to UserCollection - Remove declare module

--- src/collections/UserCollection.ts
+++ src/collections/UserCollection.ts
@@ -1,5 +1,7 @@
-import { Collection } from "@planetadeleste/vue-mc";
-import User from "../models/User";
+import { Collection } from '@planetadeleste/vue-mc';
+import User from '../models/User';
+import { Response } from '@planetadeleste/vuemc';
+import { UserData } from '@/types';
 
 export default class UserCollection extends Collection<User> {
   model() {
@@ -8,8 +10,13 @@ export default class UserCollection extends Collection<User> {
 
   routes(): Record<string, any> {
     return {
-      fetch: "users.index",
+      fetch: 'users.index',
+      list: 'users.list'
     };
+  }
+
+  async list(): Promise<Response<UserData[]>> {
+    return await this.createCustomRequest('list');
   }
 
   byActive<T extends UserCollection>(this: T): T {
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,132 +1,197 @@
 import {
-  AuthData,
-  BrandData,
-  CategoryData,
-  CurrencyData,
-  GroupData,
-  OfferData,
-  ProductData,
-  ProfileData,
-  ResponseLoginRegisterData,
-  ResponseProfileAvatarData,
-  StatData,
-  StatResponse,
-  TaxData,
-  UserAddressData,
-  UserAddressUpdateResponse,
-  UserData,
-  UserRegisterOptions,
+    AuthData,
+    BrandData,
+    CategoryData,
+    CurrencyData,
+    GroupData,
+    OfferData,
+    ProductData,
+    ProfileData,
+    ResponseLoginRegisterData,
+    ResponseProfileAvatarData,
+    StatData,
+    StatResponse,
+    TaxData,
+    UserAddressData,
+    UserAddressUpdateResponse,
+    UserData,
+    UserRegisterOptions,
 } from './types';
+import {Collection, Model} from '@planetadeleste/vue-mc';
+import {Response} from '@planetadeleste/vuemc';
 
-declare module '@planetadeleste/vue-mc-shopaholic' {
-  import { Model, Collection } from '@planetadeleste/vue-mc';
-  import { Response } from '@planetadeleste/vuemc';
+interface Auth extends Model<AuthData>, AuthData {
+}
 
-  interface Auth extends Model<AuthData>, AuthData {}
-  class Auth extends Model<AuthData> {
+class Auth extends Model<AuthData> {
     check(): Promise<Response>;
+
     csrf(): Promise<Response>;
+
     refresh(): Promise<Response>;
+
     login(login: string, password: string): Promise<Response>;
+
     logout(): Promise<Response>;
+
     register(obData: Record<string, any>): Promise<Response>;
+
     restorePassword(sEmail: string): Promise<Response>;
+
     resetPassword(
-      sCode: string,
-      sPass: string,
-      sPassConfirm: string
+        sCode: string,
+        sPass: string,
+        sPassConfirm: string
     ): Promise<Response>;
+
     checkResetCode(sCode: string): Promise<Response>;
-  }
+}
 
-  interface Brand extends Model<BrandData>, BrandData {}
-  class Brand extends Model<BrandData> {}
-  class BrandCollection extends Collection<Brand> {
+interface Brand extends Model<BrandData>, BrandData {
+}
+
+class Brand extends Model<BrandData> {
+}
+
+class BrandCollection extends Collection<Brand> {
     list(): Promise<Response<BrandData[]>>;
+
     byActive<T extends BrandCollection>(this: T): T;
-  }
+}
 
-  interface Category extends Model<CategoryData>, CategoryData {}
-  class Category extends Model<CategoryData> {}
-  class CategoryCollection extends Collection<Category> {
+interface Category extends Model<CategoryData>, CategoryData {
+}
+
+class Category extends Model<CategoryData> {
+}
+
+class CategoryCollection extends Collection<Category> {
     tree(): Promise<Response<CategoryData[]>>;
+
     list(): Promise<Response<CategoryData[]>>;
+
     byActive<T extends CategoryCollection>(this: T): T;
+
     byTree<T extends CategoryCollection>(this: T): T;
-  }
+}
 
-  interface Group extends Model<GroupData>, GroupData {}
-  class Group extends Model<GroupData> {}
-  class GroupCollection extends Collection<Group> {
+interface Group extends Model<GroupData>, GroupData {
+}
+
+class Group extends Model<GroupData> {
+}
+
+class GroupCollection extends Collection<Group> {
     list(): Promise<Response<GroupData[]>>;
-  }
+}
 
-  interface Offer extends Model<OfferData>, OfferData {}
-  class Offer extends Model<OfferData> {}
+interface Offer extends Model<OfferData>, OfferData {
+}
 
-  interface Product extends Model<ProductData>, ProductData {}
-  class Product extends Model<ProductData> {
+class Offer extends Model<OfferData> {
+}
+
+interface Product extends Model<ProductData>, ProductData {
+}
+
+class Product extends Model<ProductData> {
     stats(): Promise<Response<StatResponse>>;
+
     getOffers(): Promise<Response<OfferData[]>>;
+
     updateOffers(): Promise<void>;
-  }
-  class ProductCollection extends Collection<Product> {
+}
+
+class ProductCollection extends Collection<Product> {
     byActive<T extends ProductCollection>(this: T): T;
+
     list(): Promise<Response<ProductData[]>>;
-  }
+}
 
-  type RecordProfileData = UserRegisterOptions & Record<string, any>;
-  interface Profile extends Model<ProfileData>, ProfileData {}
-  class Profile extends Model<ProfileData> {
+type RecordProfileData = UserRegisterOptions & Record<string, any>;
+
+interface Profile extends Model<ProfileData>, ProfileData {
+}
+
+class Profile extends Model<ProfileData> {
     loadAvatar(): Promise<Response<ResponseProfileAvatarData>>;
+
     loadAddress(): Promise<Response<UserAddressData[]>>;
+
     addAddress(
-      obAddress: UserAddressData
+        obAddress: UserAddressData
     ): Promise<Response<UserAddressUpdateResponse>>;
+
     updateAddress(obAddress: UserAddressData): Promise<Response>;
+
     removeAddress(iAddressId: number): Promise<Response>;
+
     login(
-      login: string,
-      password: string
+        login: string,
+        password: string
     ): Promise<Response<ResponseLoginRegisterData>>;
+
     register(
-      obData: RecordProfileData
+        obData: RecordProfileData
     ): Promise<Response<ResponseLoginRegisterData>>;
+
     logout(): Promise<Response>;
-  }
+}
 
-  interface User extends Model<UserData>, UserData {}
-  class User extends Model<UserData> {
+interface User extends Model<UserData>, UserData {
+}
+
+class User extends Model<UserData> {
     stats(): Promise<Response<StatResponse>>;
+
     loadAddress(): Promise<Response<UserAddressData[]>>;
+
     addAddress(
-      obAddress: UserAddressData
+        obAddress: UserAddressData
     ): Promise<Response<UserAddressUpdateResponse>>;
+
     updateAddress(obAddress: UserAddressData): Promise<Response>;
+
     removeAddress(iAddressId: number): Promise<Response>;
-  }
-  class UserCollection extends Collection<User> {
+}
+
+class UserCollection extends Collection<User> {
     byActive<T extends UserCollection>(this: T): T;
-  }
 
-  interface UserAddress extends Model<UserAddressData>, UserAddressData {}
-  class UserAddress extends Model<UserAddressData> {}
+    list(): Promise<Response<UserData[]>>;
+}
 
-  interface Currency extends Model<CurrencyData>, CurrencyData {}
-  class Currency extends Model<CurrencyData> {}
-  class CurrencyCollection extends Collection<Currency> {
+interface UserAddress extends Model<UserAddressData>, UserAddressData {
+}
+
+class UserAddress extends Model<UserAddressData> {
+}
+
+interface Currency extends Model<CurrencyData>, CurrencyData {
+}
+
+class Currency extends Model<CurrencyData> {
+}
+
+class CurrencyCollection extends Collection<Currency> {
     list(): Promise<Response<CurrencyData[]>>;
+
     byActive<T extends CurrencyCollection>(this: T): T;
-  }
+}
 
-  interface Tax extends Model<TaxData>, TaxData {}
-  class Tax extends Model<TaxData> {}
-  class TaxCollection extends Collection<Tax> {
+interface Tax extends Model<TaxData>, TaxData {
+}
+
+class Tax extends Model<TaxData> {
+}
+
+class TaxCollection extends Collection<Tax> {
     list(): Promise<Response<TaxData[]>>;
-    byActive<T extends TaxCollection>(this: T): T;
-  }
 
-  export {
+    byActive<T extends TaxCollection>(this: T): T;
+}
+
+export {
     Auth,
     AuthData,
     Brand,
@@ -163,5 +228,4 @@ declare module '@planetadeleste/vue-mc-shopaholic' {
     UserData,
     UserRegisterOptions,
     UserCollection,
-  };
-}
+};
